7e22c5d6b1aea66e4be5421409dee7ce337f6e58
AnderleyCedeno <92319060+AnderleyCedeno@users.noreply.github.com>
2021-10-17 21:21:55 -0500

    Archivo realizado por el taller con el paquete Vim
    C++ codigo

---
+++ suma.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y,z;
+	cout<<"Ingrese 2 numero x y :";
+	cin>>x>>y;
+	z=x+y;
+	cout<<"El resultado es :";
+	cout<<z<<endl;
+	return 0;
+}
+
+
