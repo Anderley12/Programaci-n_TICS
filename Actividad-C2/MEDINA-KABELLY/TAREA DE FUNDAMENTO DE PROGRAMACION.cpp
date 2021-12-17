67c24bb025034aeb0a13f1e0ab6e724aff35ffd4
Kabelly Medina <92702928+kabellytame@users.noreply.github.com>
2021-12-17 17:50:28 -0500

    ACTIVIDAD C2

---
+++ Actividad-C2/MEDINA-KABELLY/TAREA DE FUNDAMENTO DE PROGRAMACION.cpp
@@ -0,0 +1,3 @@
+using namespace std;                            int main()                                      {                                               float x,y,z;                                    cout<<"ingrese el numero x : ";  cin>>x;        
+cout<<"ingrese el numero y : ";  cin>>y;        z=x+y;                                                                                          cout<<"el valor de la suma es | "<<z<<endl;     
+return  0;              
